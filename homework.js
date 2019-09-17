f4af79ebaa5e98afc6bd2b702e415db4f649f2d7
Charles Wines <ccrecorder@gmail.com>
2019-09-17 16:42:20 -0400

    moving rooms

--- homework.js
+++ homework.js
@@ -2,22 +2,54 @@
 // member of the array, and returns a new array with that member removed.
 // For example, `remove(['Cadence', 'Ordel', 'Marion'], 'Marion')` results
 // in `['Cadence', 'Ordel']`.
-//
+
 // If the potential member is not in the array, return the array unchanged.
 // If the potential member is in the array, remove all instances of it from the array.
 
+function remove(peeps_1, name) {
+	let peeps = peeps_1.slice();
+	for (let i = 0; i < peeps.length; i++) {
+		if (peeps[i] === name) {
+			peeps.splice(i, 1);
+		}
+	}
+	return peeps;
+}
 // 2. Revisit your "remove" function. Make sure that it does not change the original
 // array but instead returns a new array.
 
 // 3. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
 
+function sum(nums) {
+	let sum = 0;
+	for (let num of nums) {
+		sum += num;
+	}
+	return sum;
+}
 // 4. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
-
+function average(nums) {
+	let average = undefined;
+	let sum = 0;
+	for (let num of nums) {
+		sum += num;
+	}
+	for (let num of nums) {
+		average = sum / nums.length;
+	}
+	return average;
+}
 // 5. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
-
+function minimum(nums) {
+	let min = undefined;
+	for (let num of nums) {
+		if (min === undefined || num < min) min = num;
+	}
+	return min;
+}
 // 6. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
 // implement sorting ourselves, however.
